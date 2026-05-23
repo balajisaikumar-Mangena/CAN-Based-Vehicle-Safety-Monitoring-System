8694e1f98b1482a6f0aa154d12db1e53f72d51b9
Balaji Sai Kumar Mangena <balajisaimangena@gmail.com>
2026-05-23 22:03:40 +0530

    Main_Node

---
+++ pro_funcs.h
@@ -0,0 +1,12 @@
+void load_right_arrow(void);
+void load_left_arrow(void);
+
+void left_arrow_move(void);
+void right_arrow_move(void);
+
+void Vehicle_Dashboard(void);
+
+void Right_Indicator(void);
+void Left_Indicator(void);
+
+void vehicle(void);
